faae4725d20d977109dd8d441f8e9099d6a9f0d5
bsiddh <bsiddh667@gmail.com>
2026-02-02 21:24:23 +0530

    status code updated

--- Quizzapp/src/main/java/com/telusko/Quizzapp/Questioncontroller/QuestionController.java
+++ Quizzapp/src/main/java/com/telusko/Quizzapp/Questioncontroller/QuestionController.java
@@ -4,6 +4,8 @@ import com.telusko.Quizzapp.dao.QuestionDao;
 import com.telusko.Quizzapp.model.Question;
 import com.telusko.Quizzapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,17 +20,18 @@ public class QuestionController {
     private QuestionDao questionDao;
 
     @GetMapping("/allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
+
          return questionServices.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category){
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
+        return questionServices.getQuestionsByCategory(category);
     }
 
     @PostMapping("/add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return   questionServices.addQuestion(question);
     }
 
--- Quizzapp/src/main/java/com/telusko/Quizzapp/service/QuestionService.java
+++ Quizzapp/src/main/java/com/telusko/Quizzapp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.telusko.Quizzapp.service;
 import com.telusko.Quizzapp.dao.QuestionDao;
 import com.telusko.Quizzapp.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,12 +16,31 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-  public  List<Question> getAllQuestions(){
-      return questionDao.findAll();
+  public ResponseEntity<List<Question>> getAllQuestions(){
+      try{
+          return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK) ;
+
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST) ;
+
   }
-  public String addQuestion(Question question){
+
+  public ResponseEntity<List<Question>> getQuestionsByCategory(String category){
+     try {
+         return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+     }
+     catch (Exception e){
+         e.printStackTrace();
+     }
+     return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.BAD_REQUEST);
+
+  }
+
+  public ResponseEntity<String> addQuestion(Question question){
       questionDao.save(question);
-      return "success";
+      return new ResponseEntity<>("success",HttpStatus.CREATED);
   }
 
 }
